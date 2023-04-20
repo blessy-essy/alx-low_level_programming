6696921d1dca11000cc9c636bab6f0a4c397cdc4
blessy-essy <esthermbira000@gmail.com>
2023-04-20 09:34:05 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a line using the puts function
+ * Return: 0 (success)
+ */
+init main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
