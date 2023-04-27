100fc93cd053eaae12c3a36845c29512bbba5fd6
blessy-essy <esthermbira000@gmail.com>
2023-04-27 05:39:22 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * _isdigit - Checks for a digit
  * @c: The character to be checked
- * Return: 1 for a digit character and 0 for otherwise
+ * Return: 1 for a digit character and 0 for anything else
  */
 
 int _isdigit(int c)
 {
-	if (c <= 48 && c >= 57)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
