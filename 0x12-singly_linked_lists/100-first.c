751f25fdf1dc81009078aa921f4066eba1c5831e
blessy-essy <esthermbira000@gmail.com>
2023-06-02 15:20:20 -0700

    Prints words

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,14 @@
-#include <stdlib.h>
 #include <stdio.h>
+
+void print_words(void) __attribute__((constructor));
+
+/**
+ * print_words - prints a sentence before the main
+ * function is then to be executed
+ */
 
 void print_words(void)
 {
 	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back!\n");
 }
-
-void __attribute__((constructor)) before_main();
-
-/**
- * print_words - prints a sentence before the main
- * function is then to be executed
- */
